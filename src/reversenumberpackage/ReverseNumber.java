da94516fdd51334dbb3543ecaf5ee3bafd9a4ba0
SRC0747 <srcsrk1998@gmail.com>
2021-08-09 13:53:51 +0530

    Rever the given number.

---
+++ src/reversenumberpackage/ReverseNumber.java
@@ -0,0 +1,19 @@
+package reversenumberpackage;
+
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args)
+    {
+        int number, reverse = 0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number.");
+        number=sc.nextInt();
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
+        System.out.println("Reverse number is: " + reverse);
+    }
+}
